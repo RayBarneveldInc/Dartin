e8bb9a0d12f3da9096caf04e23ddb67fc80fc0d5
boele <boele.boom@hotmail.com>
2021-09-27 16:38:01 +0200

    Add MVVM structure with Caliburn Micro 4

---
+++ MVVM/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace MVVM
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();  
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
