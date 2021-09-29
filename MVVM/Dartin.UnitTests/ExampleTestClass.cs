3f7b348011461a653aa801415d12a957a3244625
rodeknopje <mn.stokhorst@gmail.com>
2021-09-29 10:48:39 +0200

    added unit test project

---
+++ MVVM/Dartin.UnitTests/ExampleTestClass.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class ExampleTestClass
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            Assert.Equal(1,1);
+        }
+    }
+}
