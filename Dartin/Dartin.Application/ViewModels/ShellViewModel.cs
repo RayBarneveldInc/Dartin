a5e0707a90b839aaddfec1c2bfaf4e9f9601eebf
trawzified <55751269+tr4wzified@users.noreply.github.com>
2021-09-29 14:55:13 +0200

    Remove example comment - to be added back later as a unit test

--- Dartin/Dartin.Application/ViewModels/ShellViewModel.cs
+++ Dartin/Dartin.Application/ViewModels/ShellViewModel.cs
@@ -22,27 +22,6 @@ namespace Dartin.ViewModels
                 state = JsonConvert.DeserializeObject<State>(File.ReadAllText(Path.Combine(Constants.SavePath, Constants.SaveFileName)));
             else
                 state = new State();
-
-            /*
-            Voorbeeld
-
-            var thimo = new Player("Thimo");
-            var jasper = new Player("Jasper");
-            state.Players.Add(thimo);
-            state.Players.Add(jasper);
-
-            var throw1 = new Throw(20, 3);
-            var throw2 = new Throw(20, 3);
-            var throw3 = new Throw(20, 3);
-
-            var turn1 = new Turn(thimo, new List<Throw> { throw1, throw2, throw3 });
-            var turn2 = new Turn(jasper, new List<Throw> { throw1, throw2, throw3 });
-
-            var leg = new Leg(new List<Turn>() { turn1, turn2 });
-            var set = new Set(new List<Leg> { leg });
-            state.Matches.Add(new Match(new List<Player>() { thimo, jasper }, new List<Set> { set }, "Best of 1"));
-            state.Matches[0].Name = "Best of 1 maar dan anders!";
-            */
         }
 
         public void Exit()
