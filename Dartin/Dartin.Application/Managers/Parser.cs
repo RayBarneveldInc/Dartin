f97c209c125b951db20a7d656c04be32560ed5d2
Ork <horc44@gmail.com>
2021-10-04 17:23:41 +0200

    Add parser

---
+++ Dartin/Dartin.Application/Managers/Parser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Dartin.Managers
+{
+    public static class Parser
+    {
+        public static int ParseThrow(string _throw)
+        {
+            if (_throw.ToLower() == "bull")
+            {
+                return 50;
+            }
+            else if (_throw.ToLower() == "obull")
+            {
+                return 25;
+            }
+            else
+            {
+                var score = Regex.Match(_throw, @"^(d|t|D|T)?(\d*)$", RegexOptions.IgnoreCase);
+                if (!score.Success)
+                {
+                    return -1;
+                }
+
+                var multiplier = score.Groups[1].ToString().ToLower();
+                var points = Convert.ToInt32(score.Groups[2].ToString());
+
+                if (points > 20)
+                {
+                    return -1;
+                }
+
+                if (multiplier == "d") 
+                {
+                    return points * 2; 
+                }
+
+                else if (multiplier == "t") 
+                {
+                    return points * 3; 
+                }
+
+                return points;
+            }
+        }
+    }
+}
---
+++ Dartin/Dartin.UnitTests/ParserTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Dartin.Managers;
+
+namespace UnitTests
+{
+    public class ParserTest
+    {
+        [Fact]
+        public void Parsetest()
+        {
+            Assert.Equal(20 ,Parser.ParseThrow("20"));
+            Assert.Equal(40 ,Parser.ParseThrow("D20"));
+            Assert.Equal(60 ,Parser.ParseThrow("T20"));
+            Assert.Equal(50 ,Parser.ParseThrow("Bull"));
+            Assert.Equal(25 ,Parser.ParseThrow("OBull"));
+
+            Assert.Equal(-1 ,Parser.ParseThrow("ghsjkghskj"));
+            Assert.Equal(-1 ,Parser.ParseThrow("D80"));
+            Assert.Equal(-1 ,Parser.ParseThrow("-10"));
+        }
+    }
+}
