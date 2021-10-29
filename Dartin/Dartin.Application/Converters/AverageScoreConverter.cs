bada028167919191323636e5a4764bbd32ffcd08
Silentjeerd <Tjeerdvangelder@hotmail.com>
2021-10-29 14:21:23 +0200

    Removed some comments

--- Dartin/Dartin.Application/Converters/AverageScoreConverter.cs
+++ Dartin/Dartin.Application/Converters/AverageScoreConverter.cs
@@ -27,16 +27,13 @@ namespace Dartin.Converters
                     if (legCount == 0)
                         return 0;
                     return score / legCount;
-                    //return SetScore(legs) / legs.Sum(leg => leg.Turns.Sum(turn => turn.Tosses.Count()));
 
                 case BindingList<Turn> turns:
-
                     score = turns.Sum(turn => turn.Score);
                     int turnCount = turns.Count;
                     if (turnCount == 0)
                         return 0;
                     return score / turnCount;
-                //return LegScore(turns) / turns.Sum(turn => turn.Tosses.Count());
 
                 case BindingList<Toss> tosses:
                     score = tosses.Sum(toss => toss.TotalScore);
@@ -44,7 +41,6 @@ namespace Dartin.Converters
                     if (tossCount == 0)
                         return 0;
                     return score / tossCount;
-                //return TurnScore(tosses) / tosses.Count;
                 default:
                     return 0;
             }
@@ -54,26 +50,5 @@ namespace Dartin.Converters
         {
             throw new NotImplementedException();
         }
-
-        private int SetScore(BindingList<Leg> legs)
-        {
-            int setTotal = 0;
-            foreach(Leg leg in legs) setTotal += LegScore(leg.Turns);
-            return setTotal;
-        }
-        private int LegScore(BindingList<Turn> turns)
-        {
-            int legTotal = 0;
-            foreach(Turn turn in turns) legTotal += turn.Score;
-            return legTotal;
-        }
-
-        private int TurnScore(BindingList<Toss> tosses)
-        {
-            int turnTotal = 0;
-            foreach (Toss toss in tosses) turnTotal += toss.Score;
-            return turnTotal;
-        }
-
     }
 }
